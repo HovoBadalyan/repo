ee8b4a0648af810d837fbcf2f39a50a14c562859
Hovo <h.badalyan.h@gmail.com>
2021-03-04 10:32:54 +0400

    add northweb

---
+++ NorthwndWeb/NorthwndWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwndWeb.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwndWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        [HttpGet("emps")]
+        public List<Employee> Get()
+        {
+            using NORTHWNDContext db = new
+               NORTHWNDContext();
+            return db.Employees.ToList();
+        }
+    }
+}
---
+++ NorthwndWeb/NorthwndWeb/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NorthwndWeb.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwndWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+        [HttpGet("orders")]
+        public List<Order> Get()
+        {
+            using NORTHWNDContext db = new
+               NORTHWNDContext();
+            return db.Orders.ToList();
+        }
+
+        [HttpGet("orders/{id}")]
+        public Order Get([FromRoute] int id)
+        {
+            using NORTHWNDContext db = new
+               NORTHWNDContext();
+            return db.Orders.Find(id);
+        }
+    }
+}
